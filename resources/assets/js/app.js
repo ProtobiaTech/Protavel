53b6ead4d331c60b5994ec8379ddea7f4b4214e6
郝瑞杰 <490472721@qq.com>
2017-09-20 15:15:25 +0800

    引入 element-ui  &  删除 bootstrap-sass

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,14 +8,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
+Vue.use(ElementUI);
 
 const app = new Vue({
     el: '#app'
